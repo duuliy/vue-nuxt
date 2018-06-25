70aec9ef47fc992f64bbdec62e928295b8b354c3
Yuhan zhong <715181149@qq.com>
2018-06-25 17:48:18 +0800

    Add files via upload

---
+++ 图片的几种模式/Banner.vue
@@ -0,0 +1,42 @@
+<template>
+	<!-- banner -->
+	<mt-swipe :auto="4000" class='banners' :stopPropagation="true" :showIndicators="isShow">
+		<mt-swipe-item v-for='(item,index) in banners' :key='index'>
+			<img :src="'http://www.yuzhua.com'+(item.img||item)" class='banner' :onerror="defaultBanner" />
+		</mt-swipe-item>
+	</mt-swipe>
+</template>
+
+<script type="text/javascript">
+	export default{
+		props:{
+		    banners: {},
+            isShow: {
+		        default: true
+			}
+		},
+		mounted(){
+			setTimeout(() => {
+				$('.banners div div').eq(0).addClass('is-active ')
+			}, 0);
+		},
+		data(){
+			return{
+				defaultBanner:'this.src="/img/sort/defaultBanner.png"' 
+			}
+		}
+	}
+</script>
+
+<style type="text/css" scoped>
+	.banners{
+		width: 100%;
+		height: 4.62rem;
+        background-color: #fff;
+	}
+	.banner{
+		width: 100%;
+		height: 100%;  
+		object-fit: cover;
+	}
+</style>
